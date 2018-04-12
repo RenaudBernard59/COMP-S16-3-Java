a70bf552a95de8160db10b97bdd3cd56ebc03f00
Renaud BERNARD <renaud.bernard@p59production.com>
2018-04-12 11:51:13 +0200

    Exercice 1
    Make the exercice 01 of requetes.txt it work + prepare for the rest of exercice.

--- src/test/java/streaming/test/ExerciceTest.java
+++ src/test/java/streaming/test/ExerciceTest.java
@@ -5,17 +5,181 @@
  */
 package streaming.test;
 
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.springframework.data.domain.Persistable;
+import streaming.entity.Film;
 
 /**
  *
  * @author renob
  */
 public class ExerciceTest {
-    
+    @Test
     public void req1() {
-        
+       EntityManager em = Persistence.createEntityManagerFactory("PU").createEntityManager();
+       
+       Query query = em.createQuery("SELECT f FROM Film f WHERE f.id=4");
+       Film film = (Film) query.getSingleResult();
+       
+        System.out.println(film.getTitre());
     }
-    
+    @Test
+    public void req2() {
+       EntityManager em = Persistence.createEntityManagerFactory("PU").createEntityManager();
+       
+       Query query = em.createQuery("");
+       
+       
+        System.out.println();
+    }
+//    @Test
+//    public void reqX() {
+//       EntityManager em = Persistence.createEntityManagerFactory("PU").createEntityManager();
+//       
+//       Query query = em.createQuery("");
+//       
+//       
+//        System.out.println();
+//    }
+//    @Test
+//    public void reqX() {
+//       EntityManager em = Persistence.createEntityManagerFactory("PU").createEntityManager();
+//       
+//       Query query = em.createQuery("");
+//       
+//       
+//        System.out.println();
+//    }
+//    @Test
+//    public void reqX() {
+//       EntityManager em = Persistence.createEntityManagerFactory("PU").createEntityManager();
+//       
+//       Query query = em.createQuery("");
+//       
+//       
+//        System.out.println();
+//    }
+//    @Test
+//    public void reqX() {
+//       EntityManager em = Persistence.createEntityManagerFactory("PU").createEntityManager();
+//       
+//       Query query = em.createQuery("");
+//       
+//       
+//        System.out.println();
+//    }
+//    @Test
+//    public void reqX() {
+//       EntityManager em = Persistence.createEntityManagerFactory("PU").createEntityManager();
+//       
+//       Query query = em.createQuery("");
+//       
+//       
+//        System.out.println();
+//    }
+//    @Test
+//    public void reqX() {
+//       EntityManager em = Persistence.createEntityManagerFactory("PU").createEntityManager();
+//       
+//       Query query = em.createQuery("");
+//       
+//       
+//        System.out.println();
+//    }
+//    @Test
+//    public void reqX() {
+//       EntityManager em = Persistence.createEntityManagerFactory("PU").createEntityManager();
+//       
+//       Query query = em.createQuery("");
+//       
+//       
+//        System.out.println();
+//    }
+//    @Test
+//    public void reqX() {
+//       EntityManager em = Persistence.createEntityManagerFactory("PU").createEntityManager();
+//       
+//       Query query = em.createQuery("");
+//       
+//       
+//        System.out.println();
+//    }
+//    @Test
+//    public void reqX() {
+//       EntityManager em = Persistence.createEntityManagerFactory("PU").createEntityManager();
+//       
+//       Query query = em.createQuery("");
+//       
+//       
+//        System.out.println();
+//    }
+//    @Test
+//    public void reqX() {
+//       EntityManager em = Persistence.createEntityManagerFactory("PU").createEntityManager();
+//       
+//       Query query = em.createQuery("");
+//       
+//       
+//        System.out.println();
+//    }
+//    @Test
+//    public void reqX() {
+//       EntityManager em = Persistence.createEntityManagerFactory("PU").createEntityManager();
+//       
+//       Query query = em.createQuery("");
+//       
+//       
+//        System.out.println();
+//    }
+//    @Test
+//    public void reqX() {
+//       EntityManager em = Persistence.createEntityManagerFactory("PU").createEntityManager();
+//       
+//       Query query = em.createQuery("");
+//       
+//       
+//        System.out.println();
+//    }
+//    @Test
+//    public void reqX() {
+//       EntityManager em = Persistence.createEntityManagerFactory("PU").createEntityManager();
+//       
+//       Query query = em.createQuery("");
+//       
+//       
+//        System.out.println();
+//    }
+//    @Test
+//    public void reqX() {
+//       EntityManager em = Persistence.createEntityManagerFactory("PU").createEntityManager();
+//       
+//       Query query = em.createQuery("");
+//       
+//       
+//        System.out.println();
+//    }
+//    @Test
+//    public void reqX() {
+//       EntityManager em = Persistence.createEntityManagerFactory("PU").createEntityManager();
+//       
+//       Query query = em.createQuery("");
+//       
+//       
+//        System.out.println();
+//    }
+//    @Test
+//    public void reqX() {
+//       EntityManager em = Persistence.createEntityManagerFactory("PU").createEntityManager();
+//       
+//       Query query = em.createQuery("");
+//       
+//       
+//        System.out.println();
+//    }
+
+
 }
